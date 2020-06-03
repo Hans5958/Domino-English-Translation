3a2981ba502f712cc7a5f139f967c73554213d85
Khang <khang06@users.noreply.github.com>
2020-06-03 12:17:53 -0400

    implement support for combo boxes (240)

--- compile.sh
+++ compile.sh
@@ -10,27 +10,38 @@ Log=    CON
 -delete  DIALOG,,
 -delete  STRINGTABLE,,
 -delete  VERSIONINFO,,
+-delete  240,,
 -add     Menu.res, MENU,,
 -add     Dialog.res, DIALOG,,
 -add     StringTable.res, STRINGTABLE,,
 -add     VersionInfo.res, VERSIONINFO,,
+-add     240.res, 240,,
 " > tmp.txt
 echo '"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open Dialog.rc -save Dialog.res -action compile -log CON 
 "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open Menu.rc -save Menu.res -action compile -log CON
 "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open StringTable.rc -save StringTable.res -action compile -log CON
 "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open VersionInfo.rc -save VersionInfo.res -action compile -log CON
+"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open 240.rc -save 240.res -action compile -log CON
 "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -script tmp.txt' > tmp.bat
 cat ../Dialog/* > Dialog.rc
 cat ../Menu/* > Menu.rc
 cat ../StringTable/* > StringTable.rc
 cat ../VersionInfo/* > VersionInfo.rc
+cp ../240.rc .
+mkdir -p 240
+for file in ../240/*.bin.txt
+do
+	f="$(basename -s .txt $file)"
+	python ../parsecombo.py pack $file 240/$f
+done
 sed -i '2s/.*/FILEVERSION 1,43,'$VERSION_NUM',0/' VersionInfo.rc
 sed -i '3s/.*/PRODUCTVERSION 1,43,'$VERSION_NUM',0/' VersionInfo.rc
 sed -i '12s/.*/		VALUE "FileVersion", "1.43-en.'$VERSION_NUM'-nightly.'$BUILD_DATE'"/' VersionInfo.rc
 sed -i '17s/.*/		VALUE "ProductVersion", "1.43-en.'$VERSION_NUM'-nightly.'$BUILD_DATE'"/' VersionInfo.rc
-cmd.exe /c tmp.bat
+cmd.exe /ctmp.bat
 touch Domino_Translated_$BUILD_DATE.exe
 # read -n1 -r -p "Press any key to continue..." key
 rm -rf *.rc
 rm -rf *.res
 rm -rf tmp.*
+rm -rf 240
