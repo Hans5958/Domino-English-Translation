e4b8bd5cc2e35b985035162d687e7977188e546a
Hans5958 <email5958.1@gmail.com>
2020-06-05 21:51:47 +0700

    Update scripts again 3

--- compile.sh
+++ compile.sh
@@ -20,18 +20,12 @@ Log=    CON
 -add     VersionInfo.res, VERSIONINFO,,
 -add     240.res, 240,,
 " > tmp.txt
-echo '"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open Dialog.rc -save Dialog.res -action compile -log CON 
-"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open Menu.rc -save Menu.res -action compile -log CON
-"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open StringTable.rc -save StringTable.res -action compile -log CON
-"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open VersionInfo.rc -save VersionInfo.res -action compile -log CON
-"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open 240/240.rc -save 240.res -action compile -log CON
-"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -script tmp.txt' > tmp.bat
 cat ../Dialog/* > Dialog.rc
 cat ../Menu/* > Menu.rc
 cat ../StringTable/* > StringTable.rc
 cat ../VersionInfo/* > VersionInfo.rc
 mkdir 240
-cp ../240/240.rc 240
+cp ../240/_240.rc 240
 for file in ../240/*.bin.txt; do
 	f="$(basename -s .txt $file)"
 	python3 "../_deploy/parsecombo.py" pack $file 240/$f
@@ -40,6 +34,14 @@ sed -i '2s/.*/FILEVERSION 1,43,'$VERSION_NUM',0/' VersionInfo.rc
 sed -i '3s/.*/PRODUCTVERSION 1,43,'$VERSION_NUM',0/' VersionInfo.rc
 sed -i '12s/.*/		VALUE "FileVersion", "1.43-en.'$VERSION_NUM'-dev.'$BUILD_DATE'"/' VersionInfo.rc
 sed -i '17s/.*/		VALUE "ProductVersion", "1.43-en.'$VERSION_NUM'-dev.'$BUILD_DATE'"/' VersionInfo.rc
+echo '"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open Dialog.rc -save Dialog.res -action compile -log CON
+"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open Menu.rc -save Menu.res -action compile -log CON
+"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open StringTable.rc -save StringTable.res -action compile -log CON
+"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open VersionInfo.rc -save VersionInfo.res -action compile -log CON
+cd 240
+"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open _240.rc -save ../240.res -action compile -log CON
+cd..
+"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -script tmp.txt' > tmp.bat
 cmd.exe /c tmp.bat
 touch Domino_Translated_$BUILD_DATE.exe
 rm -rf Manual
